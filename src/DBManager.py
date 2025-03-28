99c7258051d88d979ed2ce4205108df5016e4eca
bogdan.kozintsov <bogdan.kozintsov@gmail.com>
2025-03-28 13:23:00 +1100

    added: def get_companies_and_vacancies_count(self) def get_all_vacancies(self) def get_avg_salary(self) def get_vacancies_with_higher_salary(self) def get_vacancies_with_keyword(self, keyword)

--- src/DBManager.py
+++ src/DBManager.py
@@ -1,24 +1,61 @@
 import psycopg2
 
+
 class DBManager:
     """Класс для работы с БД PostgresSQL"""
-    def get_companies_and_vacancies_count():
-        """Получает список всех компаний и количество вакансий у каждой компании"""
-        pass
 
-    def get_all_vacancies():
+    def __init__(self, params):
+        self.conn = psycopg2.connect(dbname='hh', **params)
+        self.cur = self.conn.cursor()
+
+    def get_companies_and_vacancies_count(self):
+        """Получает список всех компаний и количество вакансий у каждой компании"""
+        self.cur.execute("""
+            SELECT employers.employer_name, COUNT(vacancy.employer_id)
+            FROM employers
+            JOIN vacancy USING (employer_id)
+            GROUP BY employers.employer_name
+            ORDER BY COUNT DESC
+            """)
+
+        return self.cur.fetchall()
+
+    def get_all_vacancies(self):
         """Получает список всех вакансий с указанием названия компании,
         названия вакансии и зарплаты и ссылки на вакансию"""
-        pass
+        self.cur.execute("""
+            SELECT employers.employer_name, vacancy_name, salary, vacancy_url
+            FROM vacancy
+            JOIN employers USING (employer_id)
+            ORDER BY salary desc""")
 
-    def get_avg_salary():
+        return self.cur.fetchall()
+
+    def get_avg_salary(self):
         """Получает среднюю зарплату по вакансиям"""
-        pass
+        self.cur.execute("""
+            SELECT avg(salary) from vacancy""")
 
-    def get_vacancies_with_higher_salary():
+        return self.cur.fetchall()
+
+    def get_vacancies_with_higher_salary(self):
         """Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям"""
-        pass
+        self.cur.execute("""
+            SELECT vacancy_name, salary
+            FROM vacancy
+            GROUP BY vacancy_name, salary
+            having salary > (SELECT AVG(salary) FROM vacancy)
+            ORDER BY salary DESC
+            """)
 
-    def get_vacancies_with_keyword():
+        return self.cur.fetchall()
+
+    def get_vacancies_with_keyword(self, keyword):
         """Получает список всех вакансий, в названии которых содержатся переданные в метод слова, например python"""
-        pass
+        request_sql = """
+            SELECT * FROM vacancy
+            WHERE LOWER (vacancy_name) LIKE %s
+            """
+        self.cur.execute(request_sql, ('%' + keyword.lower() + '%',))
+
+        return self.cur.fetchall()
